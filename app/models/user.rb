360edf2372fc567c424afa82a6730f199c34190f
Mohamed Hesham Othman <mohamed97198@gmail.com>
2022-01-12 00:42:46 +0200

    update templates and recipients lists dependencies with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 8..128
 
-  has_many :templates
-  has_many :recipients_lists
+  has_many :templates, dependent: :destroy
+  has_many :recipients_lists, dependent: :destroy
 end
