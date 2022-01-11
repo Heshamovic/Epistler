d4bd34e83e955849467bbfb2f1e8f9ef8552a8c1
Mohamed Hesham Othman <mohamed97198@gmail.com>
2022-01-12 00:50:15 +0200

    migrating groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220111223016) do
+ActiveRecord::Schema.define(version: 20220111224814) do
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3" do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
+  end
 
   create_table "recipients_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3" do |t|
     t.string   "name"
@@ -43,6 +51,7 @@ ActiveRecord::Schema.define(version: 20220111223016) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "recipients_lists", "users"
   add_foreign_key "templates", "users"
 end
