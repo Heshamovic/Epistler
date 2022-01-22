1f9b44464f937c9b5950f8f75c47e908f3baec38
Mohamed Hesham Othman <mohamed97198@gmail.com>
2022-01-22 23:48:12 +0200

    updating recipient and recipients_list models to allow delete when connected together
    update recipient and recipients lists models to destroy dependants when removing

--- app/models/recipients_list.rb
+++ app/models/recipients_list.rb
@@ -2,5 +2,5 @@ class RecipientsList < ApplicationRecord
   belongs_to :user
   validates :name, uniqueness: { scope: :user_id, case_sensitive: false }
   has_many :recipients_recipients_lists
-  has_many :recipients, through: :recipients_recipients_lists
+  has_many :recipients, through: :recipients_recipients_lists, dependent: :destroy
 end
